fba2630bdc7795792dd514b4486d6b7d02751f21
Prasun Thapliyal <prasun.thapliyal@gmail.com>
2018-02-28 19:10:35 +0530

    Delete node in a BST - tricky !

--- BSTInsert/BSTInsert/BSTInsert.cpp
+++ BSTInsert/BSTInsert/BSTInsert.cpp
@@ -110,6 +110,16 @@ public:
 		return FindKthSmallestInternal(k, pRoot);
 	}
 
+	Node *Delete(int k) {
+		// https://www.youtube.com/watch?v=gcULXE7ViZw
+		// https://www.geeksforgeeks.org/binary-search-tree-set-2-delete/
+		pRoot = DeleteInternalAndReturnNewRoot(k, pRoot);
+		return pRoot;
+	}
+
+	~BST() {
+		pRoot = Free(pRoot);
+	}
 private:
 	Node *pRoot;
 
@@ -184,9 +194,114 @@ private:
 
 		return NULL;
 	}
-
 	int elementIndex;
+
+	Node *DeleteInternalAndReturnNewRoot(int k, Node *pCurr) {
+		// Delete the node with key == k
+		// And return the new root of the tree
+
+		//                    5
+		//                3       25
+		//                    24      55
+		//                 9       34    63
+		//                           45
+
+		// Suppose we have to delete 5, look at the lowest in right tree and replace 5 by this value, so that 9 becomes the root. return 9
+
+		// Case 0: pCurr == NULL
+		if (!pCurr) {
+			return NULL;
+		}
+
+		// Case 1: Delete 3
+		if (pCurr->_key > k) {
+			pCurr->pLeft = DeleteInternalAndReturnNewRoot(k, pCurr->pLeft);
+			return pCurr;
+		}
+		// Case 2: Delete 24
+		if (pCurr->_key < k) {
+			pCurr->pRight = DeleteInternalAndReturnNewRoot(k, pCurr->pRight);
+			return pCurr;
+		}
+		// Case 3: Delete 5
+		if (pCurr->_key == k) {
+			// Find the min in Right sub tree == 9
+			// Replace 5's value with 9's value
+			// Delete 9 from the right subtree; the new root should be assigned as 5's right
+			if (!pCurr->pRight) {
+				Node *pLeft = pCurr->pLeft;
+				delete pCurr;
+				return pLeft;
+			}
+			if (!pCurr->pLeft) {
+				Node *pRight = pCurr->pRight;
+				delete pCurr;
+				return pRight;
+			}
+
+			Node *pMin = FindMin(pCurr->pRight);
+			pCurr->_key = pMin->_key;
+			pCurr->pRight = DeleteInternalAndReturnNewRoot(pMin->_key, pCurr->pRight);
+			return pCurr;
+		}
+
+	}
+
+	Node *FindMin(Node *p) {
+		if (!p) return NULL;
+		while (p->pLeft) {
+			p = p->pLeft;
+		}
+		return p;
+	}
+
+	Node *FindMax(Node *p) {
+		if (!p) return NULL;
+		while (p->pRight) {
+			p = p->pRight;
+		}
+		return p;
+	}
+
+	Node *Free(Node *pCurr) {
+		if (!pCurr)
+			return NULL;
+		Free(pCurr->pLeft);
+		Node *pRight = pCurr->pRight;
+		delete pCurr;
+		Free(pRight);
+		return NULL;
+	}
 };
+
+BST *GetNewBST() {
+	//                    5
+	//                3       25
+	//                    24      55
+	//                 9       34    63
+	//                           45
+
+	BST *pBST = new BST();
+	Node &pRoot =
+		pBST->Insert(5);
+	pBST->Insert(25);
+	pBST->Insert(55);
+	pBST->Insert(34);
+	pBST->Insert(24);
+	pBST->Insert(45);
+	pBST->Insert(5); // Duplicate ..
+	pBST->Insert(63);
+	pBST->Insert(9);
+	pBST->Insert(3);
+
+	return pBST;
+}
+
+void Delete(int k) {
+	BST *pBST = GetNewBST();
+	pBST->Delete(k);
+	delete pBST;
+}
 
 int main()
 {
@@ -254,6 +369,18 @@ int main()
 	pNode = bst.FindKthSmallestElement(8);
 	pNode = bst.FindKthSmallestElement(10); // Out of range - should return NULL
 
+	// Delete
+	Delete(5);
+	Delete(3);
+	Delete(25);
+	Delete(55);
+	Delete(34);
+	Delete(24);
+	Delete(45);
+	Delete(63);
+	Delete(9);
+	Delete(3);
+	Delete(11);
 	return 0;
 }
 
